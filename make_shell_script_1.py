5e7b5cd3491d0783915fb8c7e708a581bc6f64c0
GEOEGII555 <geoegii2001555@gmail.com>
2024-11-08 19:39:46 +0300

    attempt to fix linux support

--- make_shell_script_1.py
+++ make_shell_script_1.py
@@ -11,19 +11,19 @@ echo Enter the group to add the admins to, or leave blank to not add to any grou
 read admingroup
 args="--create-home"
 if [ ! -z "${usergroup}" ]; then
-    args="${args} --no-user-group -G ${usergroup}" 
+    args="${args} --no-user-group -G ${usergroup}";
 fi
 
 adminargs="${args}"
 if [ ! -z "${admingroup}" ]; then
-    adminargs="${adminargs} -G ${admingroup}"
+    adminargs="${adminargs} -G ${admingroup}";
 fi
 guestargs="${args}"
 if [ ! -z "${guestargs}" ]; then
-    guestargs="${guestargs} -G ${guestgroup}"
+    guestargs="${guestargs} -G ${guestgroup}";
 fi
 
-'''
+'''.replace("\r\n", "\n").replace("\n", "\r\n").encode('utf-8')
 
 def username_to_underscore_case(username: str) -> str:
     return username.lower().replace(" ", "_")
@@ -31,23 +31,23 @@ def username_to_underscore_case(username: str) -> str:
 with open("users.json", "r") as f:
     users: set[User] = set([User.from_dict(x) for x in json.load(f)])
 
-with open("funny_shell_script.sh", "w") as f:
+with open("funny_shell_script.sh", "wb") as f:
     f.write(SHELL_SCRIPT_HEADER)
-    f.write("\n")
+    f.write("\r\n".encode('utf-8'))
     for user in users:
         x = username_to_underscore_case(user.username)
         if repr(x) not in [f'"{x}"', f"'{x}'"] or '"' in x or "'" in x:
             continue
-        f.write(f'echo Adding user \\\"{x}\\\"... \\(Rank\\: {Rank.ELEMENTS[user.rank]}\\)\n')
-        f.write("useradd ${")
+        f.write(f'echo Adding user \\\"{x}\\\"... \\(Rank\\: {Rank.ELEMENTS[user.rank]}\\)\r\n'.encode('utf-8'))
+        f.write("useradd ${".encode('utf-8'))
         if user.rank == Rank.GUEST:
-            f.write("guest")
+            f.write("guest".encode('utf-8'))
         elif user.rank == Rank.USER:
             pass
         else:
-            f.write("admin")
-        f.write("args} ")
-        f.write(x)
-        f.write("\n")
-    f.write("pause\n")
+            f.write("admin".encode('utf-8'))
+        f.write("args} ".encode('utf-8'))
+        f.write(x.encode('utf-8'))
+        f.write("\r\n".encode('utf-8'))
+    f.write("pause\r\n".encode('utf-8'))
             
--- make_shell_script_undo_1.py
+++ make_shell_script_undo_1.py
@@ -2,7 +2,7 @@ import json
 
 from shared import User, Rank
 
-SHELL_SCRIPT_HEADER = r'#!/bin/bash'
+SHELL_SCRIPT_HEADER = r'#!/bin/bash'.encode('utf-8')
 
 def username_to_underscore_case(username: str) -> str:
     return username.lower().replace(" ", "_")
@@ -10,15 +10,14 @@ def username_to_underscore_case(username: str) -> str:
 with open("users.json", "r") as f:
     users: set[User] = set([User.from_dict(x) for x in json.load(f)])
 
-with open("undo_funny_shell_script.sh", "w") as f:
+with open("undo_funny_shell_script.sh", "wb") as f:
     f.write(SHELL_SCRIPT_HEADER)
-    f.write("\n")
+    f.write("\r\n".encode('utf-8'))
     for user in users:
         x = username_to_underscore_case(user.username)
         if repr(x) not in [f'"{x}"', f"'{x}'"] or '"' in x or "'" in x:
             continue
-        f.write(f'echo Removing user \\\"{x}\\\"... \\(Rank\\: {Rank.ELEMENTS[user.rank]}\\)\n')
-        f.write(f"userdel {x} --remove\n")
-        f.write(f"groupdel {x}\n")
-    f.write("pause\n")
-            
+        f.write(f'echo Removing user \\\"{x}\\\"... \\(Rank\\: {Rank.ELEMENTS[user.rank]}\\)\r\n'.encode('utf-8'))
+        f.write(f"userdel {x} --remove\r\n".encode('utf-8'))
+        f.write(f"groupdel {x}\r\n".encode('utf-8'))
+    f.write("pause\r\n".encode('utf-8'))
